7ea4b52848dd51e87c1b32ecac50eb629ade4bfe
venkywininfosys <venkywininfosys@gmail.com>
2016-10-20 17:31:52 -0700

    second commit

---
+++ Star.java
@@ -0,0 +1,11 @@
+public class Star{
+	public static void main (String args[]){
+		for(int i = 0; i < 6; i++){
+			for (int j = 0; j < i+1; j++){
+				System.out.print("*  ");
+			}
+			System.out.print("\n\n ");
+		}
+		
+	}		 
+}
